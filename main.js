9631d1fa7511d9319ca1b9da1e17e9279a4494ca
高橋勇輝 <takahashiyuuki@takahashiyuukinoMacBook-Pro-2.local>
2018-08-11 22:26:24 +0900

    learned watcher

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-var app = new Vue({
-    el: '#app',
-    data:{
-      budget:300,
-      limit:2,
-      list:[
-        {id: 1, name: 'apple', price: 300},
-        {id: 2, name: 'orange', price: 100},
-        {id: 3, name: 'cake', price: 200},
-        {id: 4, name: 'banana', price: 400},
-        {id: 5, name: 'grape', price: 500},
-      ],
-      order:false
-    },
-    computed:{
-      sorted: function(){
-        if(this.order){
-            $('.change').text("昇順にする");
-        } else {
-            $('.change').text("降順にする");
-        }
-        return _.orderBy(this.matched, 'price', this.order ? 'desc' : 'asc')
-      },  
-      matched: function(){
-          return this.list.filter(function(el){
-              return el.price <= this.budget
-          },this)
-      },
-      limited: function(e){
-          return this.sorted.slice(0,this.limit)
-      }
+new Vue({
+  el:"#app",
+  data:{
+    list:[],
+    current:'',
+    topics:[
+      { value: 'vue', name: 'Vue.js'},  
+      { value: 'jQuery', name: 'jQuery'}  
+    ]
+  },
+  watch: {
+    current: function(val){
+      axios.get('https://api.github.com/search/repositories',{
+        params: { q: 'topic:' + val}
+      }).then(function(response){
+        this.list = response.data.items
+      }.bind(this))
     }
+  }
 })
