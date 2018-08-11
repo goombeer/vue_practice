761cdc13e1b2b7f3c0a8f03163b017d79ef6e9fc
高橋勇輝 <takahashiyuuki@takahashiyuukinoMacBook-Pro-2.local>
2018-08-11 17:34:22 +0900

    learned computed

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 var app = new Vue({
     el: '#app',
     data:{
-      width:100,
-      height:100,
-      scrollY:0,
-      timer:null 
+      budget:300,
+      limit:2,
+      list:[
+        {id: 1, name: 'apple', price: 300},
+        {id: 2, name: 'orange', price: 100},
+        {id: 3, name: 'cake', price: 200},
+        {id: 4, name: 'banana', price: 400},
+        {id: 5, name: 'grape', price: 500},
+      ]
     },
     computed:{
-      halfwidth:{
-          get:function(){ return this.width / 2},
-          set:function(val){
-              this.width = val*2
-          }  
+      matched: function(){
+          return this.list.filter(function(el){
+              return el.price <= this.budget
+          },this)
       },
-      halfheight: function(){
-          return this.height / 2
-      },
-      halfpoint:function(){
-          return {
-              x:this.halfwidth,
-              y:this.halfheight
-          }
+      limited: function(){
+          return this.matched.slice(0,this.limit)
       }
+    },
+    methods: {
+        methodsData: function(){ return Math.random }
     }
 })
